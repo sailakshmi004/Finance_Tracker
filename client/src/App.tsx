0d498966d5d49c0d49584f2fe4555bad7156f96c
sailakshmi004 <130997937+sailakshmi004@users.noreply.github.com>
2024-09-12 21:57:53 +0530

    deploy changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Dashboard } from "./pages/dashboard";
 import { Auth } from "./pages/auth";
 import { FinancialRecordsProvider } from "./contexts/financial-record-context";
